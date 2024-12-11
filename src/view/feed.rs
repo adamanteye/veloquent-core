a66bf79f79e0db713f3bfe9dd42b840f02846328
adamanteye <yangzhe22@gmail.com>
2024-12-12 06:55:09 +0800

    feat(notification): implement new message notification utilities for groups or chats

--- src/view/feed.rs
+++ src/view/feed.rs
@@ -1,7 +1,11 @@
-use super::contact::Chat;
 use super::*;
 
-use entity::{feed, prelude::Feed};
+use entity::{
+    feed, message,
+    prelude::{Feed, Message},
+};
+
+use contact::ContactList;
 
 /// 用户或群聊的状态更新
 #[cfg_attr(feature = "dev", derive(ToSchema))]
@@ -18,21 +22,97 @@ pub struct FeedItem {
 /// 新消息通知
 #[cfg_attr(feature = "dev", derive(ToSchema))]
 #[derive(Serialize)]
-pub struct Notification {
+#[serde(tag = "type")]
+pub enum Notification {
     /// 群聊新消息
-    groups: Vec<FeedItem>,
+    Groups { feeds: Vec<FeedItem> },
     /// 双人聊天新消息
-    chats: Vec<FeedItem>,
+    Chats { feeds: Vec<FeedItem> },
+    /// 群聊新公告
+    Notices { feeds: Vec<FeedItem> },
     /// 新的希望添加自己的联系人
-    contact_requests: Vec<Chat>,
+    ContactRequests { items: ContactList },
     /// 新的被接受的联系人添加请求
-    contact_accepts: Vec<Chat>,
+    ContactAccepts { items: ContactList },
     /// 新的加入群组邀请
-    group_invites: Vec<Chat>,
+    GroupInvites { items: Vec<Uuid> },
     /// 新的希望加入自己管理的群组的请求
-    group_requests: Vec<Chat>,
-    /// 总计数, 包含新消息和新联系人/群聊申请
-    cnt: u64,
+    GroupRequests { items: Vec<Uuid> },
+}
+
+async fn count_unread_msgs(
+    user: Uuid,
+    session: Uuid,
+    notice: bool,
+    conn: &DatabaseConnection,
+) -> Result<u64, AppError> {
+    Ok(Message::find()
+        .join_rev(
+            JoinType::InnerJoin,
+            feed::Entity::belongs_to(message::Entity)
+                .from(feed::Column::Message)
+                .to(message::Column::Id)
+                .into(),
+        )
+        .filter(feed::Column::User.eq(user))
+        .filter(message::Column::Session.eq(session))
+        .filter(message::Column::Notice.eq(notice))
+        .filter(feed::Column::ReadAt.is_null())
+        .count(conn)
+        .await?)
+}
+
+impl FeedItem {
+    pub(super) async fn from_group_sessions(
+        group_sessions: &Vec<(Uuid, Uuid)>,
+        user: Uuid,
+        conn: &DatabaseConnection,
+    ) -> Result<Vec<Self>, AppError> {
+        let mut feeds = Vec::new();
+        for (group, session) in group_sessions {
+            let cnt = count_unread_msgs(user, *session, false, conn).await?;
+            feeds.push(FeedItem {
+                id: *group,
+                session: *session,
+                cnt,
+            });
+        }
+        Ok(feeds)
+    }
+
+    pub(super) async fn from_chat_sessions(
+        chat_sessions: &Vec<(Uuid, Uuid)>,
+        user: Uuid,
+        conn: &DatabaseConnection,
+    ) -> Result<Vec<Self>, AppError> {
+        let mut feeds = Vec::new();
+        for (chat, session) in chat_sessions {
+            let cnt = count_unread_msgs(user, *session, false, conn).await?;
+            feeds.push(FeedItem {
+                id: *chat,
+                session: *session,
+                cnt,
+            });
+        }
+        Ok(feeds)
+    }
+
+    pub(super) async fn from_notice_sessions(
+        notice_sessions: &Vec<(Uuid, Uuid)>,
+        user: Uuid,
+        conn: &DatabaseConnection,
+    ) -> Result<Vec<Self>, AppError> {
+        let mut feeds = Vec::new();
+        for (notice, session) in notice_sessions {
+            let cnt = count_unread_msgs(user, *session, true, conn).await?;
+            feeds.push(FeedItem {
+                id: *notice,
+                session: *session,
+                cnt,
+            });
+        }
+        Ok(feeds)
+    }
 }
 
 impl Feed {

9fdac6cc8cbb135523f4ae1b9faf0f8f8c2dad2e
adamanteye <yangzhe22@gmail.com>
2024-12-15 17:27:40 +0800

    feat: do not test find user

--- src/view.rs
+++ src/view.rs
@@ -473,14 +473,6 @@ mod tests {
             .unwrap()
     }
 
-    fn request_find_user(addr: &str, token: &str, params: &str) -> Request<Body> {
-        request_get_json()
-            .header("Authorization", format!("Bearer {token}"))
-            .uri(format!("{addr}/user{params}"))
-            .body(Body::empty())
-            .unwrap()
-    }
-
     #[tokio::test]
     async fn integration() {
         let addr = "127.0.0.1:8000";
@@ -641,9 +633,10 @@ mod tests {
             .await
             .is_ok());
         let socket_3 = Arc::new(Mutex::new(socket_3));
+        let socket = socket_2.clone();
         let task = tokio::task::spawn(async move {
             let feed_2: feed::Notification =
-                match socket_2.lock().await.next().await.unwrap().unwrap() {
+                match socket.lock().await.next().await.unwrap().unwrap() {
                     tungstenite::Message::Text(msg) => serde_json::from_str(&msg).unwrap(),
                     _ => panic!("unexpected message"),
                 };
@@ -963,6 +956,8 @@ mod tests {
             ))
             .await
             .unwrap();
+        consume_msg(socket_1.clone()).await;
+        consume_msg(socket_2.clone()).await;
         assert_eq!(response.status(), StatusCode::OK);
         tokio::time::sleep(tokio::time::Duration::from_millis(200)).await;
         // test if user can get message from contact
@@ -1025,6 +1020,7 @@ mod tests {
         )
         .unwrap();
         assert_ne!(response.token, user_1_token);
+<<<<<<< HEAD
         // test if user can find user
         let response: user::UserList = serde_json::from_reader(
             res_to_json(
@@ -1037,5 +1033,7 @@ mod tests {
         )
         .unwrap();
         assert_eq!(response.users.len(), 3);
+=======
+>>>>>>> bcb1e69 (feat: do not test find user)
     }
 }
